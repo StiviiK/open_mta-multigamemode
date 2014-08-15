56545c255c0a588792179bc07eb1150a2f596236
StiviK <stefan.kuerzeder@gmail.com>
2014-08-15 17:35:40 +0200

    Changed some uninteresting things ;)

--- mytestgamemode/init.lua
+++ mytestgamemode/init.lua
@@ -1,20 +1,17 @@
 -- #######################################
 -- ## Project:  MultiGamemode           ##
--- ## Name: 	PlayerClass Extension   ##
+-- ## Name: 	TestGamemode            ##
 -- ## Author:	StiviK                  ##
 -- ## Version: 	1.0                     ##
 -- #######################################
 
 -- This is the testgamemode for a MultiGamemode
+TestGamemode = Gamemode{nil, "TestGamemode", "This is a test gamemode!", "StiviK", nil, 32, 1, {0, 0, 0, 21}, {0, 0, 3, "cylinder", 1}} -- Register a new Gamemode
 
-newgamemode = Gamemode{nil, "TestGamemode", "This is a test gamemode!", "StiviK", nil, 32, 1, {0, 0, 0, 21}, {0, 0, 3, "cylinder", 1}} -- Register a new Gamemode
-newgamemode:addPlayer(getRandomPlayer())
-
-function newgamemode:spawnPlayer(player)
-    if (self:isPlayerInGamemode(player)) then
-		spawnPlayer(player, 0, 0, 3)
-    else
-		outputChatBox("player ist not in this gamemode!")
-    end
-end
-newgamemode:spawnPlayer(getRandomPlayer())
+addEventHandler("onMarkerHit", TestGamemode.Marker, function (ele, dim)
+	if (getElementType(ele) == "player") then
+		if (dim) then
+			TestGamemode:addPlayer(ele)
+		end
+	end
+end)
