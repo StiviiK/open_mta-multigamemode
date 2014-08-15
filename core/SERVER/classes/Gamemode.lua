40abed79c44c4ffb15106bd865bc10a1f439d28f
StiviK <stefan.kuerzeder@gmail.com>
2014-08-15 15:58:08 +0200

    Moved the Core files to /core/...

--- core/SERVER/classes/Gamemode.lua
+++ core/SERVER/classes/Gamemode.lua
@@ -178,21 +178,3 @@ function Gamemode:sendMessage (msg, r, g, b, cc)
 		outputChatBox(msg, value, r, g, b, cc)
 	end
 end
-
-
-
-
--- TESTS
-local newgamemode = Gamemode{1, "myNewGamemode", "This is a test gamemode!", nil, 32, 1, {0, 0, 0, 21}, {0, 0, 3, "cylinder", 1}} -- Register a new Gamemode
-newgamemode:addPlayer(getRandomPlayer()) -- set the player in our new gamemode
---Gamemode:getGamemodeFromID(1):addPlayer(getRandomPlayer())
-
--- "Private"-Methode (nur aufrufbar durch den spezifischen gamemode)
-function newgamemode:spawnPlayer(player)  -- add a new method to the gamemode
-    if (self:isPlayerInGamemode(player)) then -- check if the player is really in our new gamemode!
-		spawnPlayer(player, 0, 0, 3)
-    else
-		outputChatBox("player ist not in this gamemode!")
-    end
-end
-newgamemode:spawnPlayer(getRandomPlayer()) -- now we spawn the player (via the new a method from the new gamemode)
