b76fabfab796ed8b16814f4506b9e9743e901b37
StiviK <stefan.kuerzeder@gmail.com>
2014-08-15 16:22:45 +0200

    Removed Commentaries

--- mytestgamemode/init.lua
+++ mytestgamemode/init.lua
@@ -5,17 +5,16 @@
 -- ## Version: 	1.0                     ##
 -- #######################################
 
--- This is the testgamemode for a MultiGamemode-- TESTS
+-- This is the testgamemode for a MultiGamemode
 
-newgamemode = Gamemode{1, "myNewGamemode", "This is a test gamemode!", nil, 32, 1, {0, 0, 0, 21}, {0, 0, 3, "cylinder", 1}} -- Register a new Gamemode
-newgamemode:addPlayer(getRandomPlayer()) -- set the player in our new gamemode
+newgamemode = Gamemode{nil, "TestGamemode", "This is a test gamemode!", "StiviK", nil, 32, 1, {0, 0, 0, 21}, {0, 0, 3, "cylinder", 1}} -- Register a new Gamemode
+newgamemode:addPlayer(getRandomPlayer())
 
--- "Private"-Methode (nur aufrufbar durch den spezifischen gamemode)
-function newgamemode:spawnPlayer(player)  -- add a new method to the gamemode
-    if (self:isPlayerInGamemode(player)) then -- check if the player is really in our new gamemode!
+function newgamemode:spawnPlayer(player)
+    if (self:isPlayerInGamemode(player)) then
 		spawnPlayer(player, 0, 0, 3)
     else
 		outputChatBox("player ist not in this gamemode!")
     end
 end
-newgamemode:spawnPlayer(getRandomPlayer()) -- now we spawn the player (via the new a method from the new gamemode)
+newgamemode:spawnPlayer(getRandomPlayer())
