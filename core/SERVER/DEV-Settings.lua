8515e41f13f83c12c50aedc0d8654baa5c68f3ed
StiviiK <StiviiK@users.noreply.github.com>
2014-09-15 21:34:43 +0200

    Update DEV-Settings.lua

--- core/SERVER/DEV-Settings.lua
+++ core/SERVER/DEV-Settings.lua
@@ -3,11 +3,11 @@ devSettings	= {}
 -- Startup Classes (do not edit, unless you know what are you doing!)
 	-- Here you can define which classes should be started
 devSettings.startup = {
-	["Databasemanager"] = true,
-	["Playerclass"] = true,
-	["Playermanager"] = true,
-	["Gamemodemanager"] = true,
-	["Elementmanager"] = true,
-	["Permissionmanager"] = true,
-	["Mapmanager"] = true
+	["Databasemanager"] 	= true,
+	["Playerclass"] 	= true,
+	["Playermanager"] 	= true,
+	["Gamemodemanager"] 	= true,
+	["Elementmanager"] 	= true,
+	["Permissionmanager"] 	= true,
+	["Mapmanager"] 		= true
 }
