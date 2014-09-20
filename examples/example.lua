d6bb90dc33afa4c74fc35cd96ec7d0c1b6c66ef0
StiviK <trash.stivi@gmail.com>
2014-09-20 15:06:52 +0200

    Improved Example (only German)

--- examples/example.lua
+++ examples/example.lua
@@ -1,20 +1,26 @@
--- #######################################
--- ## Project:  MultiGamemode           ##
--- ## Name: 	EXAMPLE                 ##
--- ## Author:	StiviK                  ##
--- ## Version: 	1.0                     ##
--- #######################################
-
--- This is the testgamemode for a MultiGamemode-- TESTS
-newgamemode = Gamemode{1, "myNewGamemode", "This is a test gamemode!", nil, 32, 1, {0, 0, 0, 21}, {0, 0, 3, "cylinder", 1}} -- Register a new Gamemode
-newgamemode:addPlayer(getRandomPlayer()) -- set the player in our new gamemode
-
--- "Private"-Methode (nur aufrufbar durch den spezifischen gamemode)
-function newgamemode:spawnPlayer(player)  -- add a new method to the gamemode
-    if (self:isPlayerInGamemode(player)) then -- check if the player is really in our new gamemode!
-		spawnPlayer(player, 0, 0, 3)
-    else
-		outputChatBox("player ist not in this gamemode!")
-    end
+Example = Gamemode{nil, "Example", "Example Gamemode", "StiviK", nil, 16, 2, {}, {}} -- Registrieren wir einen neuen Gamemode
+--[[ Registrier Argumente
+1: nil: so wird die ID automatisch zu gewiesen
+2: "Example": Dies ist der Name des Gamemodes
+3: "Example Gamemode": Dies ist die beschreibung des Gamemodes
+4: "StiviK": Dies ist der Autor des Gamemodes
+5: nil: so wird die Diemension für die Spieler automatisch zugewiesen
+6: 16: max. Spieler anzahl
+7: 2: benötigte Spieler anzahl damit der Gamemode weiter ausgeführt wird
+8: {}: Blip Argumente, müssen in einer Table sein! (hier eine leere Table damit kein Blip erstellt wird)
+9: {}: Marker Argumente, müssen in einer Table sein! (hier eine leere Table damit kein Marker erstellt wird)
+--]] nil: so wird auch die Dimension automatisch zugewiesen
+function Example:sayHelloToThePlayer (player) -- Dass ist jetzt eine Custom funktion, die lassen wir aufrufen wenn ein spieler den Gamemode betritt^
+	if isElement(player) then -- validieren wir ob es wirklich ein spieler ist
+		outputChatBox(("Guten Tag, %s! Wie geht's?"):format(getPlayerName(player)), player) -- senden wir ihm eine Nachricht!
+	end
 end
-newgamemode:spawnPlayer(getRandomPlayer()) -- now we spawn the player (via the new a method from the new gamemode)
+
+function Example:onPlayerJoin (player) -- Diese Funktion wird von dem Gamemodemanager aufgerufen wenn ein Spieler den Gamemode betritt
+	Example:sayHelloToThePlayer(player) -- rufen wir unsere Funktion auf, die nur der Gamemode "Basejump" hat und aufrufen kann
+end
+
+addCommandHandler("joinBasejump", function (player)
+	player:getGamemode():removePlayer(player) -- entfehren wir den Spieler aus seinem Aktuellen Gamemode
+	Example:addPlayer(player) -- jetzt setzen wir ihn in unseren Basejump Gamemode
+end)
