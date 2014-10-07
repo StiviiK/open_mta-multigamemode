4aa66f5629a34c68f3c8d5ed6da89ec09993324d
StiviiK <stefan.kuerzeder@gmail.com>
2014-10-07 20:59:56 +0200

    Added a localPlayer Class

---
+++ core/CLIENT/classes/localPlayer.lua
@@ -0,0 +1,15 @@
+-- Count FPS
+localPlayer.fps = {FPS = 0, realFPS = 0, lastTick = getTickCount(), lastTick = getTickCount()}
+addEventHandler("onClientRender", root, function ()
+	if (getTickCount() - localPlayer.fps.lastTick) >= 1000 then
+		localPlayer.fps.lastTick = getTickCount()
+		localPlayer.fps.realFPS = localPlayer.fps.FPS
+		localPlayer.fps.FPS = 0
+	end
+	
+	localPlayer.fps.FPS = localPlayer.fps.FPS + 1
+end)
+
+function localPlayer:getFPS ()
+	return localPlayer.fps.realFPS
+end
